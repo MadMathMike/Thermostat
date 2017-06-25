57ec65480a1f430cfcd20905f354752fd2534ec7
MadMathMike <madmathmike@gmail.com>
2017-06-25 21:56:57 +0000

    add support for second shift register in twodigitoutput module. added test method that simply counts up to a certain number

--- twodigitoutput.py
+++ twodigitoutput.py
@@ -1,39 +1,52 @@
 import shiftpi
 
 shiftpi.pinsSetup(18, 23, 24)
+shiftpi.shiftRegisters(2)
 shiftpi.startupMode(shiftpi.LOW, True)
 
-def _writeDigit(value):
+def writeNumber(number):
+  firstDigit = (number % 100)/10
+  secondDigit = number % 10
+
   shiftpi.digitalWrite(shiftpi.ALL, shiftpi.LOW)
+  _writeDigit(firstDigit)
+  _writeDigit(secondDigit, 1)
+
+def _writeDigit(value, position = 0):
+  pins = [];
 
   if value == 0:
-    _digitalWriteHigh([6, 1, 2, 3, 4, 5])
+    pins = [6, 1, 2, 3, 4, 5]
   if value == 1:
-    _digitalWriteHigh([1, 2])
+    pins = [1, 2]
   if value == 2:
-    _digitalWriteHigh([6, 1, 7, 3, 4])
+    pins = [6, 1, 7, 3, 4]
   if value == 3:
-    _digitalWriteHigh([6, 1, 7, 2, 3])
+    pins = [6, 1, 7, 2, 3]
   if value == 4:
-    _digitalWriteHigh([5, 7, 1, 2])
+    pins = [5, 7, 1, 2]
   if value == 5:
-    _digitalWriteHigh([6, 5, 7, 2, 3])
+    pins = [6, 5, 7, 2, 3]
   if value == 6:
-    _digitalWriteHigh([6, 5, 4, 3, 2, 7])
+    pins = [6, 5, 4, 3, 2, 7]
   if value == 7:
-    _digitalWriteHigh([6, 1, 2])
+    pins = [6, 1, 2]
   if value == 8:
-    _digitalWriteHigh([1, 2, 3, 4, 5, 6, 7])
+    pins = [1, 2, 3, 4, 5, 6, 7]
   if value == 9:
-    _digitalWriteHigh([1, 6, 5, 7, 2])
+    pins = [1, 6, 5, 7, 2]
 
-def _digitalWriteHigh(pins):
   for pin in pins:
-    shiftpi.digitalWrite(pin, shiftpi.HIGH)
+    shiftpi.digitalWrite(pin + position * 8, shiftpi.HIGH)
 
-for digit in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
-  _writeDigit(digit)
-  shiftpi.delay(500)
+def countToN(n):
+  number = 0
 
-shiftpi.digitalWrite(shiftpi.ALL, shiftpi.LOW)
+  while number <= n:
+    writeNumber(number)
+    shiftpi.delay(250)
+    number += 1
+
+  shiftpi.delay(1000)
+  shiftpi.digitalWrite(shiftpi.ALL, shiftpi.LOW)
 
