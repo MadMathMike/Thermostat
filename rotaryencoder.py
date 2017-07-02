047bd0c3cefe802d3be0e1455ea51d920c0b965d
MadMathMike <madmathmike@gmail.com>
2017-07-02 23:03:00 +0000

    Grabbed rotary encoder python code from SunFire (needs tweaking to fit my needs)

---
+++ rotaryencoder.py
@@ -0,0 +1,63 @@
+#Lifted from SunFounder code: https://www.sunfounder.com/learn/Super_Kit_V2_for_RaspberryPi/lesson-8-rotary-encoder-super-kit-for-raspberrypi.html
+import RPi.GPIO as GPIO
+import time
+
+RoAPin = 17
+RoBPin = 27
+RoSPin = 22
+
+globalCounter = 0
+
+flag = 0
+Last_RoB_Status = 0
+Current_RoB_Status = 0
+
+def setup():
+	GPIO.setmode(GPIO.BCM)       # Numbers GPIOs by physical location
+	GPIO.setup(RoAPin, GPIO.IN)    # input mode
+	GPIO.setup(RoBPin, GPIO.IN)
+	GPIO.setup(RoSPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+	rotaryClear()
+
+def rotaryDeal():
+	global flag
+	global Last_RoB_Status
+	global Current_RoB_Status
+	global globalCounter
+	Last_RoB_Status = GPIO.input(RoBPin)
+	while(not GPIO.input(RoAPin)):
+		Current_RoB_Status = GPIO.input(RoBPin)
+		flag = 1
+	if flag == 1:
+		flag = 0
+		if (Last_RoB_Status == 0) and (Current_RoB_Status == 1):
+			globalCounter = globalCounter + 1
+			print 'globalCounter = %d' % globalCounter
+		if (Last_RoB_Status == 1) and (Current_RoB_Status == 0):
+			globalCounter = globalCounter - 1
+			print 'globalCounter = %d' % globalCounter
+
+def clear(ev=None):
+        globalCounter = 0
+	print 'globalCounter = %d' % globalounter
+	time.sleep(1)
+
+def rotaryClear():
+        GPIO.add_event_detect(RoSPin, GPIO.FALLING, callback=clear) # wait for falling
+
+
+def loop():
+	global globalCounter
+	while True:
+		rotaryDeal()
+#		print 'globalCounter = %d' % globalCounter
+
+def destroy():
+	GPIO.cleanup()             # Release resource
+
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
